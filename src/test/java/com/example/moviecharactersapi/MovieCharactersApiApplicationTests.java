0d4fc8a477e10f2747371bc9c3726cbdf167af5c
Joakim Österberg <joakimosterberg@hotmail.com>
2021-07-26 12:39:21 +0200

    Initial commit

---
+++ src/test/java/com/example/moviecharactersapi/MovieCharactersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviecharactersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCharactersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
